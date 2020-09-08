68a9de5b15794ac06106b0a6b644841bfc676a3a
John O'Loughlin <johnoloughlin33@outlook.com>
2020-09-08 18:08:44 -0500

    yeeahaw 2

--- week 2 homework.js
+++ week 2 homework.js
@@ -4,13 +4,13 @@ let numberOfToppings = 8;
 console.log(pizzaPlace, typeof pizzaPlace);
 console.log(numberOfToppings, typeof numberOfToppings);
 
-console.log("I own ${pizzaPlace} and we have ${numberOfToppings} toppings.");
+console.log("We are ${pizzaPlace}, we offer ${numberOfToppings} toppings.");
 
 if (numberOfToppings < 10) {
   console.log("Quality, not quantity.");
 } else {
   console.log("A whole lot of pizza.");
 }
-for (let i = 1; i <= numberOfToppings; i++) {
+for (let i = 1; i <= numberOfToppings; i+) {
   console.log(i);
 }
