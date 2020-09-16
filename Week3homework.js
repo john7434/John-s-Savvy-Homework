e21c09cd552f2cd58352d64418a573785b27e3f4
John O'Loughlin <johnoloughlin33@outlook.com>
2020-09-16 18:45:03 -0500

    Week 3 Homework added

---
+++ Week3homework.js
@@ -0,0 +1,28 @@
+const pizzaToppings = ["Pepperlonely", "BBQ", "Apple", "kickin chicken"];
+
+console.log("Welcome to my pie domain, heres what you can get:");
+for (let toppingsList of pizzaToppings) {
+  console.log(toppingsList);
+}
+const pizzaOrder = {
+  size: ["8 Inch", "12 Inch", "16 Inch"],
+  crust: ["Normal", "Pan"],
+  toppings: []
+};
+console.log(
+  pizzaOrder.size[1],
+  pizzaOrder.crust[0],
+  "pizza with",
+  pizzaToppings[0],
+  pizzaToppings[2],
+  "is firing up!"
+);
+const preparePizza = {
+  size: pizzaOrder.size[1],
+  crust: pizzaOrder.crust[0],
+  toppings: [pizzaToppings[0], pizzaToppings[2]]
+};
+console.log("In the oven");
+const pizza = [preparePizza.size, preparePizza.crust, preparePizza.toppings];
+console.log("Pipin ready");
+console.log("Hope you like your", pizza, "Pizza.");
